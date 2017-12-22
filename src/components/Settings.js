f6fc24ab7520c5fc5e93be9258c1be44b75e3434
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-22 19:20:53 +0100

    Captured value from input

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -4,13 +4,36 @@ import {connect} from "react-redux";
 
 
 export default class Settings extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        console.log(this.state.value);
+    }
+
     render(){
         return (
             <div id="settings">
-            <h3>Settings</h3>
-                <div class="form-group">
-                    <input type="number" className="form-control" placeholder="Enter New Timer"/>
-                </div>          
+                <h3>Settings</h3>
+                <form onSubmit={evt => this.handleSubmit(evt)}>
+                    <div className="form-group">
+                        <input type="number" 
+                        className="form-control" 
+                        placeholder="Enter New Timer"
+                        onChange={this.handleChange}/>
+                    </div>
+                    <button type="submit" value="" className="btn btn-primary">Save</button>
+                </form>        
             </div>
         );
     }
