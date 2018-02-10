a5c6a7d9637f7a64f796179bff55526c4a2c1b62
Zuhair Rayyes <zuz1995@yahoo.com>
2018-02-10 10:08:52 +0100

    added new colors to timers

--- src/components/TimerItem.js
+++ src/components/TimerItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import C from "../constants";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 let candleCounter = 1;
 let minute = 0;
@@ -13,52 +13,52 @@ class TimerItem extends Component {
         // Start at 9:30
         let hourStart = 9;
         let minuteStart = 30;
-        
+
         // Check if it's the 60 minute timer and start at 9:00
-        if (this.props.minute === 60){
+        if (this.props.minute === 60) {
             hourStart = 9;
             minuteStart = 0;
         }
 
         let time = this.props.est.split(":");
-        candleCounter = Math.floor((((time[0]-hourStart) * 60) + (time[1]-minuteStart)) / this.props.minute) + 1;
-        minute = this.props.minute - (((time[0] - hourStart) * 60 + (time[1] - minuteStart))%(this.props.minute)) - 1;
-        seconds = 60-time[2];
+        candleCounter = Math.floor((((time[0] - hourStart) * 60) + (time[1] - minuteStart)) / this.props.minute) + 1;
+        minute = this.props.minute - (((time[0] - hourStart) * 60 + (time[1] - minuteStart)) % (this.props.minute)) - 1;
+        seconds = 60 - time[2];
     };
 
     // Play a sound or vibrate when reaching the final minute in a timer
-    notify(){
-        if(this.props.vibrate && ((minute === 0) && (seconds === 59))){
+    notify() {
+        if (this.props.vibrate && ((minute === 0) && (seconds === 59))) {
             // navigator.vibrate(300);
         }
     }
-    
+
     render() {
         this.doMath();
         this.notify();
         return (
             <div className="boxy unselectable">
                 {/* Check if last or first minute */}
-                <li className={(minute === 0) ?
-                    "list-group-item list-group-item-danger" :
-                    (minute === (this.props.minute - 1)) ? 
-                    "list-group-item list-group-item-success" :  
-                    "list-group-item list-group-item-warning"}>
-                    {(minute > 30) ? 
-                    `${this.props.minute} | ${minute} | ${candleCounter}` :
-                    `${this.props.minute} | ${minute}:${seconds} | ${candleCounter}`}
+                <li className={(minute === 0)
+                    ? "list-group-item list-group-item-red"
+                    : (minute === 1)
+                        ? "list-group-item list-group-item-orange"
+                        : "list-group-item list-group-item-green"}>
+                    {(minute > 30)
+                        ? `${this.props.minute} | ${minute} | ${candleCounter}`
+                        : `${this.props.minute} | ${minute}:${seconds} | ${candleCounter}`}
                 </li>
             </div>
         );
     }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     return {
         est: state.est,
         sound: state.sound,
         vibrate: state.vibrate
     };
-  }
-  
+}
+
 export default connect(mapStateToProps)(TimerItem);
