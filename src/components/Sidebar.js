6206eff2dc7c602f5724281c7b9c0cdbe7567f64
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-03-08 18:00:57 +0100

    Added form to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { slide as Menu } from 'react-burger-menu';
+import MenuItem from 'material-ui/MenuItem';
+
+import { reduxForm, Field } from 'redux-form';
+import { RadioButton } from 'material-ui/RadioButton';
+import {
+    Checkbox,
+    RadioButtonGroup,
+    SelectField,
+    TextField,
+    Toggle,
+    DatePicker
+} from 'redux-form-material-ui';
 
 class Sidebar extends Component {
 
     render = () => (
         <Menu isOpen={false}>
-            <a id="home" className="menu-item" href="/">Home</a>
-            <a id="about" className="menu-item" href="/about">About</a>
-            <a id="contact" className="menu-item" href="/contact">Contact</a>
-            <a onClick={this.showSettings} className="menu-item--small" href="">Settings</a>
+            <form>
+                <Field name="username" component={TextField} hintText="Street" />
+
+                <Field name="plan" component={SelectField} hintText="Select a plan">
+                    <MenuItem value="monthly" primaryText="Monthly" />
+                    <MenuItem value="yearly" primaryText="Yearly" />
+                    <MenuItem value="lifetime" primaryText="Lifetime" />
+                </Field>
+
+                <Field name="agreeToTerms" component={Checkbox} label="Agree to terms?" />
+
+                <Field name="eventDate" component={DatePicker} format={null} hintText="What day is the event?" />
+
+                <Field name="receiveEmails" component={Toggle} label="Please spam me!" />
+
+                <Field name="bestFramework" component={RadioButtonGroup}>
+                    <RadioButton value="react" label="React" />
+                    <RadioButton value="angular" label="Angular" />
+                    <RadioButton value="ember" label="Ember" />
+                </Field>
+            </form>
         </Menu>
     );
 }
 
 
-function mapStateToProps(state) {
-    return {};
-}
-
-export default connect(mapStateToProps)(Sidebar);
+Sidebar = reduxForm({
+    form: 'Sidebar'
+  })(Sidebar)
+  
+  export default Sidebar
