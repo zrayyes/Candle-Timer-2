aacdd94364b27bec33859228b6a3051fe5a1742a
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-03-08 16:29:40 +0100

    Added basic sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,42 @@
-import React,{ Component } from 'react';
-import {connect} from "react-redux";
-
-// import C from "../constants";
-
+import React, { Component } from 'react';
+import { connect } from "react-redux";
 import TimerList from "./TimerList";
-// import Settings from "./Settings";
-
-// TODO: Add row with cols
+import { slide as Menu } from 'react-burger-menu'
 
 class App extends Component {
+
+  showSettings(event) {
+    event.preventDefault();
+    console.log('click');
+  }
+
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div >
-            {/* <h1>Candle Timer</h1> */}
-            <h1>EST: {this.props.est}</h1>            
+      <div>
+        <Menu isOpen={false}>
+          <a id="home" className="menu-item" href="/">Home</a>
+          <a id="about" className="menu-item" href="/about">About</a>
+          <a id="contact" className="menu-item" href="/contact">Contact</a>
+          <a onClick={this.showSettings} className="menu-item--small" href="">Settings</a>
+        </Menu>
+        <div className="container">
+          <div className="row">
+            <div >
+              {/* <h1>Candle Timer</h1> */}
+              <h1>EST: {this.props.est}</h1>
+            </div>
           </div>
+          <br />
+          <TimerList />
         </div>
-        <br/>
-        <TimerList />
       </div>
     );
   }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-      est: state.est
+    est: state.est
   };
 }
 
