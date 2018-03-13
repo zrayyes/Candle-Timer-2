19ed30ef56d225ff169f22910090f10855dc86e4
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-03-13 11:18:25 +0100

    Changed sound initial state

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,12 +29,15 @@ class Header extends Component {
     render = () => (
         <div>
             <h1>EST: {this.state.time}</h1>
+            <p>{this.props.sound}</p>
         </div>
     );
 }
 
 function mapStateToProps(state) {
-    return {};
+    return {
+        sound: state.sound
+    };
 }
 
 export default connect(mapStateToProps)(Header);
